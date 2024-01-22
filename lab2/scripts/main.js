d5b1e1cb93b94e5734b2e5a50e4299e338c21e6b
Christian <pngue006@uottawa.ca>
2024-01-21 22:40:57 -0500

    Main page + pagination

--- lab2/scripts/main.js
+++ lab2/scripts/main.js
@@ -1,3 +1,9 @@
+// Unhide home page on load
+window.onload = function() {
+    const home = document.querySelector('#products');
+    home.classList.toggle('hidden');
+}
+
 // Fetch the JSON data
 fetch("./scripts/products.json")
     .then(response => response.json())
@@ -18,13 +24,31 @@ fetch("./scripts/products.json")
 
             // Set the item's HTML
             itemElement.innerHTML = `
-                <img src="${item.image}" alt="IMAGE">
+                <img src="${item.image}" class="image" alt="IMAGE">
+                <div class="description">
                 <p class="title">${item.item}</p>
                 <p class="price">${item.price}</p>
-                <span class="add-button reveal"><p>ADD TO CART</p></span>
+                </div>
+                <span class="add-button reveal"><p>+</p></span>
             `;
 
             // Add the item element to the product grid
             productGrid.appendChild(itemElement);
-        });
     });
+});
+
+function toggleSection(sectionId) {
+    // Toggle hidden for every other section
+    const allSections = document.querySelectorAll('.content .section');
+    allSections.forEach((sec) => { 
+        if (!sec.classList.contains('hidden')) {
+            sec.classList.add('hidden');
+        }
+    });
+    
+    // Get the section element
+    const sectionElement = document.querySelector(`#${sectionId}`);
+
+    // Toggle the section
+    sectionElement.classList.toggle('hidden');
+}
