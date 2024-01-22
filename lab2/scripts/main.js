d81f1d2f01947dae67b370fbadb81efff51d9213
Christian <christian0draws@gmail.com>
2024-01-22 14:11:36 -0500

    preferences page layout

--- lab2/scripts/main.js
+++ lab2/scripts/main.js
@@ -18,24 +18,50 @@ fetch("./scripts/products.json")
 
         // Loop through the items
         data.forEach(item => {
-            // Create a new item element
-            const itemElement = document.createElement('div');
-            itemElement.className = 'item reveal';
+        // Create a new item element
+        const itemElement = document.createElement('div');
+        itemElement.className = 'item';
 
-            // Set the item's HTML
-            itemElement.innerHTML = `
-                <img src="${item.image}" class="image" alt="IMAGE">
-                <div class="description">
-                <p class="title">${item.item}</p>
-                <p class="price">${item.price}</p>
-                </div>
-                <span class="add-button reveal"><p>+</p></span>
-            `;
+    
+        // Set the item's HTML
+        itemElement.innerHTML = `
+        <img src="${item.image}" class="image" alt="IMAGE">
+        <div class="description">
+        <p class="title">${item.item}</p>
+        <p class="price">${item.price}</p>
+        </div>
+        <span class="add-button reveal"><p>+</p></span>
+    `
 
-            // Add the item element to the product grid
-            productGrid.appendChild(itemElement);
-    });
+        // Add the item element to the product grid
+        productGrid.appendChild(itemElement);
+        })
 });
+
+fetch("./scripts/preferences.json")
+.then(response => response.json())
+.then(data => {
+    // get diet-selection element
+    const dietSelection = document.querySelector('.diet-selection');
+
+    // empty div
+    while(dietSelection.firstChild) {
+        dietSelection.firstChild.remove();
+    }
+
+    data.forEach(item => {
+        const diet = document.createElement('div');
+
+        diet.innerHTML = `
+            <input type="checkbox" id="${item}" name="${item}" value="${item}">
+            <label for="${item}">${item}</label>
+        `;
+
+        // Add each item to the selection list
+        dietSelection.appendChild(diet);
+    })
+
+})
 
 function toggleSection(sectionId) {
     // Toggle hidden for every other section
