fe0d6819e7cf41f3744e21cbf683cbdf41be0ee9
Christian <pngue006@uottawa.ca>
2024-02-09 20:49:10 -0500

    Hotfix

--- lab4/scripts/utils.js
+++ lab4/scripts/utils.js
@@ -1,19 +1,26 @@
 $(document).ready(function() {
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
+    // Select the links
+    const links = document.querySelectorAll('a');
+
+    // Add a click event listener to each link
+    links.forEach((link) => {
+        link.addEventListener('click', (event) => {
+            // Prevent the default action
             event.preventDefault();
-            // Store hash
-            var hash = this.hash;
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({ scrollTop: $(hash).offset().top }, 100, function() {
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
+
+            // Get the target element
+            const targetId = event.target.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+
+            // Calculate the position to scroll to
+            const positionToScrollTo = targetElement.getBoundingClientRect().top + window.pageYOffset - 85;
+
+            // Scroll to the target element
+            window.scrollTo({
+                top: positionToScrollTo,
+                behavior: 'smooth'
             });
-        } // End if
+        });
     });
 
     $("#scrolltop").on('click', function () {
