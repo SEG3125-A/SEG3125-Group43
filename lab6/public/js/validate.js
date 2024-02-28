83f7b18f3de92e74bccbe5b7923b386de0657397
Christian <pngue006@uottawa.ca>
2024-02-27 21:08:42 -0500

    Responsiveness Update

--- lab6/public/js/validate.js
+++
@@ -1,9 +0,0 @@
-// Custom functions used to validate form elements 
-
-// TODO Validate checkboxes
-
-// TODO Validate radio buttons
-
-// TODO Pop-up alert for submit button
-
-// TODO Pop-up alert for general invalid form
